1aafb02ea82c73b1f658dcbd6dce214efc31f78e
david.umiri <david.umirieduca.madrid.org>
2022-10-27 19:53:44 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    ejercicios/html/fechaActual.html -> ejercicios/Act 4.1/index.html
    	new file:   ejercicios/Act 4.2/index.html
    	new file:   ejercicios/Act 4.3/index.html
    	renamed:    ejercicios/html/4_4_index.html -> ejercicios/Act 4.4/4_4_index.html
    	new file:   ejercicios/Act 4.4/script.js
    	deleted:    ejercicios/html/apuntesArrays.html
    	deleted:    ejercicios/html/apuntesDOM.html
    	deleted:    ejercicios/html/arrayDiezNombres.html
    	deleted:    ejercicios/html/buscarPalabra.html
    	deleted:    ejercicios/html/childNodes.html
    	deleted:    ejercicios/html/ejercicioDNI.html
    	deleted:    ejercicios/html/ejercicioDNI2.html
    	deleted:    ejercicios/html/ejercicioMap.html
    	deleted:    ejercicios/html/ejerciciosUnidad2.html
    	deleted:    ejercicios/html/horaTitulo.html
    	deleted:    ejercicios/html/integracionDeCodigo.html
    	deleted:    ejercicios/html/nombreEdad.html
    	deleted:    ejercicios/html/nombreEdadMap.html
    	deleted:    ejercicios/html/setFunciones.html
    	deleted:    ejercicios/html/setIntervalDNI.html
    	deleted:    ejercicios/js/apuntesU3.js
    	deleted:    ejercicios/js/buscarPalabra.js
    	deleted:    ejercicios/js/childNodes.js
    	deleted:    ejercicios/js/integracionDeCodigo.js
    
    27/10/2022

---
+++ ejercicios/Act 4.4/script.js
@@ -0,0 +1,7 @@
+// elemento id input2
+document.getElementById("input2");
+document.querySelector("label #input2");
+
+// la coleccion de parrafos
+document.getElementById("lipsum").children;
+document.querySelector("div").children;
--- ejercicios/js/integracionDeCodigo.js
+++
@@ -1,2 +0,0 @@
-alert("hola mundo 2");
-console.log("integracion de codigo en archivo externo");
