dc5eaed2847714afd4883b5374d124ec0bc8f5ae
david.umiri <davidumiricondo@gmail.com>
2023-02-03 13:39:01 +0100

    03/02/2023

--- vuejs/API-German/src/paginas/paginaListar.vue
+++ vuejs/API-German/src/paginas/paginaListar.vue
@@ -1,16 +1,21 @@
+
 <script setup>
+
 import contenido from '../components/contenido.vue'
+
 </script>
+
 
 <template>
     <div>
         <h1>Página Listar</h1>
         <div>fasdfasdfasdfasdfasdfasdfasdfasdffa </div>
         <div>fasdfasdfasdfasdfasdfasdfasdfasdffa </div>
-        
-        <contenido parametro="x"/>
+
+        <contenido parametro="x" />
 
     </div>
+
 </template>
 
 <style scoped>
