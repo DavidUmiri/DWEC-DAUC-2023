4ae7a9a5098a6f5d96ca954655c6f456d321ef87
david.umiri <davidumiricondo@gmail.com>
2023-02-17 22:08:50 +0100

    17/02/2023

--- ejercicios/segunda_evaluación/FormularioEjerciciosResueltos/Ejemplo-Acceso/js/formulario.js
+++ ejercicios/segunda_evaluación/FormularioEjerciciosResueltos/Ejemplo-Acceso/js/formulario.js
@@ -3,79 +3,72 @@
         Rellenar el valor del campo nombre utilizando un "prompt" para introducir los datos
 *********************************************************************************************
 *********************************************************************************************/
-document.getElementById("id_input_text").addEventListener("click", (event) => {
-  // utilizando el objeto event
-  // event.target.value = prompt("Introduce tu nombre: ");
 
-  // utilizando el objeto this
-  this.document.forms["id_form"].elements["id_input_text"].value = prompt(
-    "Introduce tu nombre: "
-  );
-  // this.document.forms["id_form"]["id_input_text"].value = prompt("Introduce tu nombre: ");
-
-  console.log(this.document.forms["id_form"].elements["id_input_text"].value);
+document.getElementById('id_input_text').addEventListener("click",(event) => {
+  event.target.value = prompt("Introduce tu nombre: ");
 });
+
 
 /*********************************************************************************************
 *********************************************************************************************
     Modificar el color del fondo del formulario
 *********************************************************************************************
 *********************************************************************************************/
-for (let e of document.getElementsByName("form-color")) {
-  e.addEventListener("click", (event) => {
-    document.getElementById("id_form").style.backgroundColor =
-      event.target.value;
+for(let e of document.getElementsByName('form-color')){
+  e.addEventListener("click",(event) => {
+    document.getElementById('id_form').style.backgroundColor = event.target.value;
   });
-}
+}  
+
 
 /*********************************************************************************************
 *********************************************************************************************
      Resetear el formulario
 *********************************************************************************************
 *********************************************************************************************/
-document.forms[0]["reset"].addEventListener("click", function () {
+document.forms[0]['reset'].addEventListener("click",function() {
+  
   alert("Empezamos de nuevo");
-  document.getElementById("answers").innerHTML = "<h2>Respuestas</h2>";
-  document.getElementById("id_form").style.backgroundColor = "white"; // lo colocamos en blanco cuando borramos
+  document.getElementById("answers").innerHTML="<h2>Respuestas</h2>";
+ 
 });
+
 
 /*********************************************************************************************
 *********************************************************************************************
       Recoger, recorrer y mostrar el contenido del formulario
 *********************************************************************************************
 *********************************************************************************************/
-document.forms[0].addEventListener("submit", (event) => {
-  document.getElementById("answers").innerHTML = "<h2>Respuestas</h2>";
-
-  for (let e of document.forms[0].elements) {
+document.forms[0].addEventListener("submit",(event) => {
+  
+  document.getElementById("answers").innerHTML="<h2>Respuestas</h2>";
+  
+  for(let e of document.forms[0].elements){
     let contentText = "";
     let container = document.createElement("p");
-
-    switch (e.type) {
+    
+    switch(e.type){
       case "checkbox":
-        if (e.checked) contentText += `${e.name}: ${e.value}`;
+        if(e.checked) contentText+=`${e.name}: ${e.value}`;
         break;
       case "radio":
-        if (e.checked) contentText += `${e.name}: ${e.value}`;
-        break;
+        if(e.checked) contentText+=`${e.name}: ${e.value}`;
+        break;  
       case "color":
-        if (e.value != "") contentText += `${e.name}: ${e.value}`;
-        break;
+        if(e.value!="") contentText+=`${e.name}: ${e.value}`;
+        break;  
       default:
-        if (e.value != "" && e.type != "submit" && e.type != "reset")
-          contentText += `${e.name}: ${e.value}`;
-        break;
+        if(e.value!="" && e.type!="submit" && e.type!="reset") contentText+=`${e.name}: ${e.value}`;
+        break;    
     }
 
-    // TODO: condición de que si el valor es vacío no recoja los datos
-
-    if (e.type == "color") container.style.color = `${e.value}`;
+    if(e.type=="color") container.style.color = `${e.value}`;
     container.appendChild(document.createTextNode(contentText));
     document.getElementById("answers").appendChild(container);
   }
 
   event.preventDefault();
-});
+})
 
 /*********************************************************************************************
 *********************************************************************************************
@@ -84,28 +77,27 @@ document.forms[0].addEventListener("submit", (event) => {
 *********************************************************************************************/
 let dias = new Set();
 
-for (let e of document.getElementsByClassName("day-available")) {
-  e.addEventListener("click", (event) => {
-    let diaMarcado = event.target.id.substring(9);
+for(let e of document.getElementsByClassName('day-available')){
 
-    if (dias.has(diaMarcado)) {
+  e.addEventListener("click",(event) => {
+    let diaMarcado = event.target.id.substring(9);
+    if(dias.has(diaMarcado)){
       dias.delete(diaMarcado);
-    } else {
+    }else{
       dias.add(diaMarcado);
     }
-    let selectDiaPreferente = document.forms[0].elements["id_select"];
-
-    if (dias.size == 0) {
-      selectDiaPreferente.innerHTML =
-        '<option value="" selected disabled>Ningún elemento seleccionado</option>';
-    } else {
+    let selectDiaPreferente = document.forms[0].elements['id_select'];
+    
+    if(dias.size == 0 ){
+      selectDiaPreferente.innerHTML = '<option value="" selected disabled>Ningún elemento seleccionado</option>';
+    }else{
       selectDiaPreferente.innerHTML = "";
     }
 
     let i = 0;
-    for (d of dias) {
+    for(d of dias){
       let textday = "";
-      switch (d) {
+      switch(d){
         case "monday":
           textday = "Lunes";
           break;
@@ -122,20 +114,17 @@ for (let e of document.getElementsByClassName("day-available")) {
           textday = "Viernes";
           break;
         default:
-          break;
+          break;          
       }
-
-      let contentText = document.createTextNode(textday);
+      let contentText = document.createTextNode(textday);  
       let container = document.createElement("option");
       container.appendChild(contentText);
       container.value = d;
-
-      if (i == dias.size - 1) {
+      if(i == dias.size - 1 ){
         container.selected = true;
       }
-
       selectDiaPreferente.appendChild(container);
       i++;
     }
   });
-}
+}  
